76c2f5753514430b204f549958e8789ca93672c5
zxzr <3020908652@qq.com>
2020-02-17 16:22:09 +0800

    c

--- drone_fix.py
+++ drone_fix.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import re as regex
+import re
 import sys
 
 
@@ -14,19 +14,19 @@ def txtomd():
     with open(sys.argv[1], 'r') as f:
         listfile = f.readlines()
 
-    rbj1 = regex.compile(r"第\d章")
-    rbj2 = regex.compile(r"\d.\d[.]?\d")
-    rbj3 = regex.compile(r"\d.\d")
-    nospace = regex.compile(r"\s*")
+    rbj1 = re.compile(r"第\d章")
+    rbj2 = re.compile(r"\d.\d[.]?\d")
+    rbj3 = re.compile(r"\d.\d")
+    nospace = re.compile(r"\s*")
     print("[TOC]")
     for t in listfile:
-        tt = regex.sub(rbj1, "# ", t)
-        tt = regex.sub(rbj2, "### ", tt)
-        tt = regex.sub(rbj3, "## ", tt)
-        tt = regex.sub(r"复习题", "", tt)
-        tt = regex.sub(r"参考文献", "", tt)
-        tt = regex.sub(nospace, "", tt)
-        tt = regex.sub(r"(^#+)", r"\1 ", tt)
+        tt = re.sub(rbj1, "# ", t)
+        tt = re.sub(rbj2, "### ", tt)
+        tt = re.sub(rbj3, "## ", tt)
+        tt = re.sub(r"复习题", "", tt)
+        tt = re.sub(r"参考文献", "", tt)
+        tt = re.sub(nospace, "", tt)
+        tt = re.sub(r"(^#+)", r"\1 ", tt)
         if tt:
             print(tt)
 
